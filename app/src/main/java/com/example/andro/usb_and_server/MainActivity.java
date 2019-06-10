29498a4a64e937ae0524008111ab83ab5c33c451
Andro <https://github.com/AndroGamal>
2019-06-11 02:28:34 +0300

    web-server-android

--- app/src/main/java/com/example/andro/usb_and_server/MainActivity.java
+++ app/src/main/java/com/example/andro/usb_and_server/MainActivity.java
@@ -2,12 +2,10 @@ package com.example.andro.usb_and_server;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,7 +16,6 @@ import java.net.Socket;
 public class MainActivity extends AppCompatActivity {
     Button s;
     ServerSocket serverSocket;
-    // Process p;
 EditText email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -308,7 +305,6 @@ EditText email,password;
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (Exception e) {
-                Log.e("error", e.getMessage());
             }
         }
     }
